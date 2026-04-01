68f93e559b76ed1da8bd1ecc91e8dc85b5ece20c
Dmitriy <ppshhh.15@yandex.ru>
2026-04-01 23:01:41 +0300

    Обновлены фÐайлы main.py, setup.py и .gitignore

--- setup.py
+++ setup.py
@@ -1,24 +1,39 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="langbot",
+    name="LangBot",
     version="1.0.0",
     description="Telegram-бот для изучения английского языка",
-    author="Nekrasov N.A.;Dmitriy O.A.",
+    author="Nekrasov N.A.; Ovchinnikov D.A.",
     packages=find_packages(),
+
     # Основные зависимости для работы бота
     install_requires=[
         "pyTelegramBotAPI",
         "python-dotenv"
     ],
+
     # Дополнительные зависимости для сборки документации
     extras_require={
-
+        "docs": [
+            "sphinx>=7.0",
+            "sphinx-rtd-theme",
+            "sphinx-copybutton",
+            "myst-parser"
+        ],
+        "dev": [
+            "sphinx>=7.0",
+            "sphinx-rtd-theme",
+            "sphinx-copybutton",
+            "myst-parser",
+            "pytest"
+        ]
     },
-    setup_requires=[
-    ],
-    python_requires=">=3.10",
+
+    python_requires=">=3.12",
+
     project_urls={
         "Documentation": "",
+        "Source": "https://github.com/nnekrasov1/LangBot"
     },
 )
